9f75d0cc9dccf7140075b3b8ae35176208a64ce4
Titas Lukosius <titasgitara@gmail.com>
2021-11-14 19:10:01 +0200

    6: data fetch in component

--- js/components/CarGridComponent.js
+++ js/components/CarGridComponent.js
@@ -1,10 +1,36 @@
 class CarGridComponent { 
     constructor() {
+        this.state = {
+            loading: false,
+            cars: []
+        }
         this.init();
     }
 
+    fetchCars = () => API.fetchCars(this.saveCars, alert);
+
+    saveCars = (cars) => {
+        this.state.cars = cars;
+        this.state.loading = false;
+
+        this.render();
+    }
+
     init = () => {
+        this.state.loading = true;
+        this.fetchCars();
         this.htmlElement = document.createElement('div');
-        this.htmlElement.innerHTML = 'As Testukas tu Tevukas';
+
+        this.render();
+    }
+
+    render = () => {
+        const {loading, cars} = this.state;
+        if(loading) {
+            this.htmlElement.innerHTML = 'Siunciama...'
+        } else {
+            this.htmlElement.innerHTML = 'Parsiusta'
+
+        }
     }
 }
